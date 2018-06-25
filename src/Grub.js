f57c51196b4ef555d372b1124fd2efcb125fcea6
Ricardo <rroque98@gmail.com>
2018-06-24 18:25:42 -0700

    Added es6 grub class. Passes all tests.

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,11 @@
 class Grub {
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat () {
+    this.food = null;
+    return 'Mmmmmmmmm jelly'
+  }
 };
